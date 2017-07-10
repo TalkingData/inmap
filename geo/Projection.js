24cb73957c32a724481a2818169fec0da49b097a
fenglu.li <fenglu.li@tendcloud.com>
2017-07-10 11:29:22 +0800

    update

--- geo/Projection.js
+++
@@ -1,9 +0,0 @@
-export class Projection {
-    constructor() {}
-    lngLatToPoint() {
-        throw "lngLatToPoint方法未实现";
-    }
-    pointToLngLat() {
-        throw "pointToLngLat方法未实现";
-    }
-}
