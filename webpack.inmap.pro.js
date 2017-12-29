8e5034631cc0f6e9752c9ac29aa146d5037f2210
fenglu.li <fenglu.li@tendcloud.com>
2017-12-29 11:54:28 +0800

    Revert "add"
    This reverts commit 3988b130ea728c04790b75dca95b0835fdab26fd.

--- webpack.inmap.pro.js
+++ webpack.inmap.pro.js
@@ -2,7 +2,7 @@
  * Created by lu on 2016/12/5.
  */
 const merge = require('webpack-merge');
-const config = require('./webpack.inmap');
+const config = require('./webpak.inmap');
 const webpack = require('webpack');
 const path = require('path');
 
