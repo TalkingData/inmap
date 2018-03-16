3e10b2efd2e3f90353b26892f22aa45263716839
fenglu.li <fenglu.li@tendcloud.com>
2018-03-16 11:38:23 +0800

    优化打包

--- build-worker.js
+++ build-worker.js
@@ -1,25 +1,24 @@
-
-
 let path = require('path');
 let fs = require('fs');
-let workerContent = fs.readFileSync(path.join(__dirname, './dist/worker.min.js'), 'utf-8');
+let workerPath = './dist/worker.min.js';
+let workerContent = fs.readFileSync(path.join(__dirname, workerPath), 'utf-8');
 
 fs.readFile(path.join(__dirname, './dist/inmap.min.js'), 'utf-8', function (err, data) {
     if (err) {
         throw new Error('file not find');
     } else {
 
-        workerContent =workerContent.replace(/"/g,'\\"').replace(/'/g,'\\\'');
-        
+        workerContent = workerContent.replace(/"/g, '\\"').replace(/'/g, '\\\'');
+
         let repaleData = data.replace('[workerContentString]', workerContent);
         fs.writeFile(path.join(__dirname, './dist/inmap.min.js'), repaleData, function (err) {
             if (err) {
                 throw new Error('write error');
             }
-        });      
+        });
     }
 
 });
-
-
-
+fs.unlink(workerPath, (err) => {
+    if (err) throw err;
+});
