3590bb543774498c8bf8e791f1a14ca2713e86d5
fenglu.li <fenglu.li@tendcloud.com>
2018-05-28 18:32:24 +0800

    优化CircuitOverlay
    支持GeoJOSN 地理编码格式
    优化一下其他

--- src/overlay/CircuitOverlay.js
+++ src/overlay/CircuitOverlay.js
@@ -19,16 +19,23 @@ export class CircuitOverlay extends CanvasOverlay {
         this.points = [];
         this.styleConfig = {};
         this._setStyle(CircuitConfig, ops);
-        this._isCoordinates = false;
         this.state = null;
         this.workerData = [];
     }
     _setStyle(config, ops) {
         let option = merge(config, ops);
-        this.points = ops.data ? option.data : this.points;
+        if (ops.data) {
+            this.setData(ops.data);
+        } else {
+            this.map && this.refresh();
+        }
         this.styleConfig = option.style;
         this.eventConfig = option.event;
         this.tMapStyle(option.skin);
+    }
+    setOptionStyle(ops) {
+        this._setStyle(CircuitConfig, ops);
+        this.map && this.drawMap();
     }
     setState(val) {
         this.state = val;
@@ -36,39 +43,31 @@ export class CircuitOverlay extends CanvasOverlay {
     }
     translation(distanceX, distanceY) {
         for (let i = 0; i < this.workerData.length; i++) {
-            let pixels = this.workerData[i].pixels;
+            let pixels = this.workerData[i].geometry.pixels;
             for (let j = 0; j < pixels.length; j++) {
                 let pixel = pixels[j];
                 pixel[0] = pixel[0] + distanceX;
                 pixel[1] = pixel[1] + distanceY;
             }
         }
-        this.setState(State.drawBefore);
-        this.drawLine(this.workerData);
-        this.setState(State.drawAfter);
+        this.refresh();
     }
 
-    resize() {
-        this.drawMap();
-    }
-    setOptionStyle(ops) {
-        this._setStyle(CircuitConfig, ops);
-        this.coordinates(this.points);
-        this.drawMap();
-    }
     setData(points) {
-        this.setPoints(points);
-    }
-    setPoints(points) {
         if (!isArray(points)) {
             throw new TypeError('inMap: data must be a Array');
         }
         this.points = points;
-
-        this.coordinates(this.points);
+        this.map && this.drawMap();
+    }
+    refresh() {
+        this.setState(State.drawBefore);
+        this.drawLine(this.workerData);
+        this.setState(State.drawAfter);
+    }
+    resize() {
         this.drawMap();
     }
-
     drawMap() {
         let zoomUnit = Math.pow(2, 18 - this.map.getZoom());
         let projection = this.map.getMapType().getProjection();
@@ -79,83 +78,43 @@ export class CircuitOverlay extends CanvasOverlay {
             nwMc: nwMc,
             zoomUnit: zoomUnit
         };
-        if (!this._isCoordinates) {
-            this.coordinates(this.points);
-        }
+
         this.setState(State.computeBefore);
         this.postMessage('CircuitOverlay.calculatePixel', params, (pixels, margin) => {
             if (this.eventType == 'onmoving') {
                 return;
             }
             this.setState(State.conputeAfter);
-            this.clearCanvas();
             clearPushArray(this.workerData, pixels);
             this.translation(margin.left - this.margin.left, margin.top - this.margin.top);
             params = null;
             margin = null;
         });
     }
-
-
-    coordinates(data) {
-        this._isCoordinates = true;
-        let projection = this.map.getMapType().getProjection();
-        for (let i = 0; i < data.length; i++) {
-            let item = data[i];
-            item['_coordinates'] = item.geo.map(function (item) {
-
-                let pixel = projection.lngLatToPoint({
-                    lng: item[0],
-                    lat: item[1]
-                });
-                return [pixel.x, pixel.y];
-            });
-
-        }
-
-
-    }
-    transferCoordinate(_coordinates, nwMc, zoomUnit) {
-
-        return _coordinates.map(function (item) {
-
-            let x = (item[0] - nwMc.x) / zoomUnit;
-            let y = (nwMc.y - item[1]) / zoomUnit;
-            return [x, y];
-        });
-    }
-    lngLatToPoints(data, nwMc, zoomUnit) {
-        if (data.length > 0) {
-            return this.transferCoordinate(data, nwMc, zoomUnit);
-        } else {
-            return [];
-        }
-    }
-
-    drawLine(data) {
+    drawLine() {
         this.clearCanvas();
         let normal = this.styleConfig.normal;
-        this.ctx.shadowBlur = 0;
-        this.ctx.shadowOffsetX = 0;
-        this.ctx.shadowOffsetY = 0;
-        this.ctx.lineCap = 'butt';
-        this.ctx.lineJoin = 'miter';
-        this.ctx.globalCompositeOperation = 'lighter';
-        this.ctx.miterLimit = 10;
-        this.ctx.strokeStyle = normal.borderColor;
-        this.ctx.lineWidth = normal.borderWidth;
-        this.ctx.beginPath();
+        let ctx = this.ctx;
+        ctx.shadowBlur = 0;
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 0;
 
-        for (let i = 0; i < data.length; i++) {
-            let item = data[i];
+        if (normal.globalCompositeOperation) {
+            ctx.globalCompositeOperation = normal.globalCompositeOperation;
+        }
 
-            let pixels = item.pixels;
-            this.ctx.moveTo(pixels[0][0], pixels[0][1]);
+        for (let i = 0; i < this.workerData.length; i++) {
+            let item = this.workerData[i];
+            this.ctx.strokeStyle = normal.borderColor;
+            let pixels = item.geometry.pixels;
+            ctx.beginPath();
+            ctx.moveTo(pixels[0][0], pixels[0][1]);
             for (let j = 1; j < pixels.length; j++) {
-                this.ctx.lineTo(pixels[j][0], pixels[j][1]);
+                ctx.lineTo(pixels[j][0], pixels[j][1]);
             }
-            this.ctx.stroke();
-
+            ctx.lineWidth = normal.borderWidth;
+            pixels = null;
+            ctx.stroke();
         }
 
 
