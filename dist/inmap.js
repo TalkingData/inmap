abedfa94cc1d3ff72693d167c1dda461d6873ca7
fenglu.li <fenglu.li@tendcloud.com>
2018-05-29 18:45:48 +0800

    add

--- dist/inmap.js
+++ dist/inmap.js
@@ -5588,7 +5588,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var version = "1.5.8";
+var version = "1.5.9";
 console.log('inMap v' + version);
 
 var inMap = {
