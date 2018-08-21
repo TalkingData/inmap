5107fc83e988f5162393780fbd09cc9693db5142
fenglu.li <fenglu.li@tendcloud.com>
2018-08-21 15:52:02 +0800

    LineStringOverlay 修改

--- src/overlay/LineStringOverlay.js
+++ src/overlay/LineStringOverlay.js
@@ -105,10 +105,16 @@ export default class LineStringOverlay extends Parameter {
         }
     }
     setData(points) {
-        if (!isArray(points)) {
-            throw new TypeError('inMap: data must be a Array');
+        if (points) {
+            if (!isArray(points)) {
+                throw new TypeError('inMap: data must be a Array');
+            }
+            this.points = points;
+        } else {
+            this.points = [];
         }
-        this.points = points;
+        this.clearData();
+        this.cancerSelectd();
         this.map && this.drawMap();
     }
     refresh() {
